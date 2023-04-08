9f6474a0d8a97f2d27e2ca3d13c64b27138bfeed
dongomes <gomesedgar69@gmail.com>
2023-04-08 12:40:57 -0400

    ejercicios adicionales

--- exercise_20.php
+++ exercise_20.php
@@ -5,13 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Template</title>
+    <title>Tipos de datos en php</title>
 </head>
 <body>
   <?php
-    ECHO "Mi primer script de php!";
-    echo "Mi primer script de php!";
-    EcHo  "Mi primer script de php!";
+    /*
+    Las variables pueden almacenar datos de diferentes tipos y diferentes tipos de datos pueden realizar operaciones distintas
+    */
+
+    // PHP admite los siguientes tipos de datos:
+    /*
+    -string
+    -integer
+    -float (numero de coma flotante o tambien llamado double o decimal)
+    -boolean
+    -array
+    -object
+    -Null
+    -Resource
+    */
+
+    //PHP float example
+    $x= 5.25;
+    var_dump($x);
+    echo "<br>";
+
+    //PHP boolean
+    $y=true;
+    echo $y;
+    echo "<br>";
+    var_dump($y);
+
+    //PHP array
+    $colores= array("azul","amarillo","rojo");
+    var_dump($colores);
   ?>
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
---
+++ exercise_21.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Object php</title>
+</head>
+<body>
+  <?php
+    class Carro {
+      public $color;
+      private $anio;
+      protected $nombre;
+
+      public function __construct($color, $anio) {
+        $this -> color = $color;
+        $this -> anio = $anio;
+      }
+
+      public function mensaje(){
+        return 'Mi carro es color ' . $this -> color . ' y año ' . $this -> anio . '!';
+      }
+    }
+
+    class Modelo extends Carro{
+      public function __construct($color,$nombre){
+        $this ->nombre =$nombre;
+        $this ->color=$color;
+      }
+
+      public function mensaje2(){
+        return 'Mi carro, aparte de ser color' . $this->color . ' es modelo ' . $this->nombre . '!';
+      }
+    }
+
+    $miCarro= new Carro("plateado", 2015);
+    echo $miCarro->mensaje();
+
+    echo'<br>';
+
+    $miCarro= new Carro ("rojo", 2008);
+    echo $miCarro->mensaje();
+
+    echo '<br>';
+
+    $miModelo= new Modelo ('rojo', 'chevrolet');
+    echo $miModelo->mensaje2();
+
+    echo '<br>';
+
+    $miModelo=new Modelo ('blanco', 'toyota');
+    echo $miModelo->mensaje2();
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
